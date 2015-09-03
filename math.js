265853584a33e670e7944c05c545cf1818afa45f
Kimberly Regan <kregan16@gmail.com>
2015-09-03 13:48:03 -0400

    fixed links

--- math.js
+++ math.js
@@ -20,9 +20,9 @@ function single_problem() {
                     '</table>';
                   
     }
-      for (i=0; i<3; i++){
+      for (i=0; i<5; i++){
         document.write("<tr>");
-        for (j=0; j<3; j++) {
+        for (j=0; j<5; j++) {
           document.write('<td>' + single_problem() + '</td>');
         }
         document.write('</tr>');
